3ce17e54f41b963296ec705d6c9c940cfc98baab
Kqan1 <kaaneskimarmara@gmail.com>
2023-11-24 21:12:21 +0300

    update

--- components/search/SearchMenu.tsx
+++ components/search/SearchMenu.tsx
@@ -2,14 +2,27 @@ import { forwardRef } from "react";
 
 const SearchMenu = forwardRef<HTMLDivElement>((props, ref) => {
     return (
-        <div className="fixed inset-0 h-screen w-screen flex items-center justify-center bg-zinc-600/30">
-            <div
-                className="h-5/6 w-4/6 border-2
-                border-black"
-                ref={ref}
-                {...props}
-            >
+        <div className="fixed inset-0 h-screen w-screen z-40 flex items-center justify-center bg-zinc-700/80">
+            <div className="h-5/6 w-5/6 flex space-x-4" ref={ref} {...props} >
                 
+                {/* 2 horizontonal Parts */}
+                <div className="h-full w-4/12 space-y-4">
+                    {/* 2 vertical parts */}
+                
+                    <div className="h-1/6 rounded bg-zinc-200 border-2 border-zinc-500/70">
+                        test1
+                    </div>
+                    
+                    <div className="h-[calc(83.333333%-1rem)] rounded bg-zinc-200 border-2 border-zinc-500/70">
+                        test2
+                    </div>
+                
+                </div>
+                
+                <div className="h-full w-8/12 rounded bg-zinc-200 border-2 border-zinc-500/70">
+                    test3
+                </div>
+            
             </div>
         </div>
     );
--- components/search/index.tsx
+++ components/search/index.tsx
@@ -4,11 +4,12 @@ import SearchMenu from "./SearchMenu";
 export default function Search() {
     const [searchMenuOpen, setSearchMenuOpen] = useState(false);
 
-    const searchIconRef = useRef(null);
+    const searchIconRef = useRef<SVGSVGElement>(null);
+    const pathRef = useRef<SVGPathElement>(null);
     const menuRef = useRef<HTMLDivElement>(null);
     useEffect(() => {
         function handleOutsideClick(e: MouseEvent) {
-            if ( menuRef.current && !menuRef.current.contains(e.target as Node) && searchIconRef.current !== e.target ) {
+            if ( menuRef.current && !menuRef.current.contains(e.target as Node) && searchIconRef.current !== e.target  && pathRef.current !== e.target ) {
                 setSearchMenuOpen(false);
             };
         };
@@ -21,7 +22,7 @@ export default function Search() {
         <div className="h-4/6">
             <svg className="h-full p-0.5 aspect-square cursor-pointer fill-zinc-700/50 hover:fill-zinc-500 dark:fill-zinc-200/50 hover:dark:fill-zinc-300 transition-colors"
                 onClick={() => setSearchMenuOpen(!searchMenuOpen)}
-                ref={searchIconRef} viewBox="0 0 512 512"><path ref={searchIconRef} d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
+                ref={searchIconRef} viewBox="0 0 512 512"><path ref={pathRef} d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
             {searchMenuOpen && <SearchMenu ref={menuRef} />}
         </div>
     );
