066d4f340bfa4a7351cc2c27f6bbb0f5857faff3
Kqan1 <kaaneskimarmara@gmail.com>
2023-11-28 17:41:41 +0300

    update

--- config/s3.ts
+++
@@ -1,2 +0,0 @@
-export const ALLOWED_FILE_TYPES = ['image/png', 'image/jpeg'];
-export const S3_BUCKET_NAME = 'image-to-alt';
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,6 @@
 import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-import { ALLOWED_FILE_TYPES } from '@/config/s3';
-
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
-};
-
-export function validateFileType(file: File) {
-    return ALLOWED_FILE_TYPES.includes(file.type);
 };
